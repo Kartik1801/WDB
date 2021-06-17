62cf580ed19c6127adfa5fd6f4828ce1d26fb3c2
KD1801 <kartikdhawan1801@gmail.com>
2021-06-18 00:41:46 +0530

    update

--- JS/TVSHOWAPI/assets/app.js
+++ JS/TVSHOWAPI/assets/app.js
@@ -3,5 +3,15 @@ form.addEventListener('submit',async function (e){
 e.preventDefault();
 const searchinput=form.elements.showsearch.value;
 const  res= await axios.get(`http://api.tvmaze.com/search/shows?q=${searchinput}`)
-console.log(res.data);
+getImg(res.data);
 })
+
+const getImg= (shows) =>{
+    for(let result of shows){
+    if(result.show.image){
+    const img=document.createElement("img");
+    img.src=result.show.image.medium;
+    document.querySelector("body").append(img);
+    }
+}
+}
