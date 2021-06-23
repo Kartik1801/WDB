a831a946143d9c0bf2c0ec009f517dd8024d4d2c
KD1801 <kartikdhawan1801@gmail.com>
2021-06-24 00:40:34 +0530

    Updated

--- JS/TVSHOWAPI/assets/app.js
+++ JS/TVSHOWAPI/assets/app.js
@@ -2,36 +2,48 @@ const form= document.querySelector("#searchform");
 
 form.addEventListener('submit',async function (e){
     e.preventDefault();
-    const input=form.elements.query;
-    const searchinput=input.value;
-    //const  res= await axios.get(`http://api.tvmaze.com/search/shows?q=${searchinput}`)
-    const param={ params: { q: searchinput } }
+    
+    const SearchResult=document.querySelector("h2");
+    const r=document.querySelectorAll(".res");
+    //remove all the result cards if any 
+    r.forEach((elem)=>elem.remove());
+    //remove any result Text If any
+    SearchResult.innerText="";
+    //get Search Query
+    const searchInput=form.elements.query.value;
+    //Get parameters to send to URL
+    const param={ params: { q: searchInput } }
+    //Send Request to requested APIs
     const  res= await axios.get(`http://api.tvmaze.com/search/shows`,param);
-    showResult(searchinput,res.data);
+    //Function Call to display result obtained
+    showResult(searchInput,res.data);
+    //Clears the Text input
     input.value="";
-
 })
-const showResult= (searchinput,searchresult)=>{
-    const sr=document.querySelector("h2");
+const showResult= (searchInput,searchresult)=>{
+    // To display Search Result Text
+    const SearchResult=document.querySelector("h2");
     if(searchresult===[]){
-        sr.innerText=`No Such Show Found <O_O>...`
+        SearchResult.innerText=`No Such Show Found <O_O>...`
     }
     else
     {
-        const main=document.querySelector(".main");
-    sr.innerText=`Search Result for "${searchinput}"`;
-    getImg(res.data)
+    const main=document.querySelector(".main");
+    SearchResult.innerText=`Search Result for "${searchInput}"`;
+    //Function call to Create & Display cards for all results:
+    getData(searchresult)
     }
 }
-const getImg= (shows) =>{
-    for(let result of shows){
-    if(result.show.image){
-
-    const img=document.createElement("img");
-    img.src=result.show.image.medium;
-    document.querySelector("body").append(img);
+const getData= (shows) =>{
+    for(let show of shows){
+        
+     if(show.show.image)
+     {
+        const img=document.createElement("img");
+        img.src=result.show.image.medium;
+        document.querySelector("body").append(img);
+     }
     }
-}
 }
 
 //TO DO 
