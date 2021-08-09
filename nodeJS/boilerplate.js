f30a2bac972168e8084995fa2926e18a979ad037
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-09 23:04:53 +0530

    Script to create directory with boilerplate

--- nodeJS/boilerplate.js
+++ nodeJS/boilerplate.js
@@ -1,7 +1,7 @@
 // Script to create Directory based on passed arguments with html css and js files.
- 
-const fs = require('fs');
+ const fs = require('fs');
 const folderName= process.argv.slice(2)
+try{
 folderName.forEach(element => {
     fs.mkdirSync(element);
     fs.writeFileSync(`${element}/index.html`)
@@ -9,3 +9,8 @@ folderName.forEach(element => {
     fs.writeFileSync(`${element}/Assets/app.css`) 
     fs.writeFileSync(`${element}/Assets/app.js`) 
 });
+}
+catch(e){
+    console.log("Something is wrong!");
+    console.log(e);
+}
