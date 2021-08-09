b504827f6208b93ded5f259b9ba828a69fbe7313
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-09 23:01:05 +0530

    Updated

--- nodeJS/boilerplate.js
+++ nodeJS/boilerplate.js
@@ -1,2 +1,11 @@
+// Script to create Directory based on passed arguments with html css and js files.
+ 
 const fs = require('fs');
-fs.mkdir('/usr/bon/node',{})
+const folderName= process.argv.slice(2)
+folderName.forEach(element => {
+    fs.mkdirSync(element);
+    fs.writeFileSync(`${element}/index.html`)
+    fs.mkdirSync(`${element}/Assets`);
+    fs.writeFileSync(`${element}/Assets/app.css`) 
+    fs.writeFileSync(`${element}/Assets/app.js`) 
+});
