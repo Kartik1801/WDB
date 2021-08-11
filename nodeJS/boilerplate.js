1d6a204bfa8f0a773afb6f4f0bf96affb99ee3d1
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-11 22:16:32 +0530

    Fixed

--- nodeJS/boilerplate.js
+++ nodeJS/boilerplate.js
@@ -2,7 +2,16 @@
 
 const fs = require('fs');
 const folderName= process.argv.slice(2)
-console.log(process .argv)
+try{
 folderName.forEach(element => {
     fs.mkdirSync(element);
+    fs.writeFileSync(`${element}/index.html`)
+    fs.mkdirSync(`${element}/Assets`);
+    fs.writeFileSync(`${element}/Assets/app.css`) 
+    fs.writeFileSync(`${element}/Assets/app.js`) 
 });
+}
+catch(e){
+    console.log("Something is wrong!");
+    console.log(e);
+}
