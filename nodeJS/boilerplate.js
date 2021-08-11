f0fcb8dce6ef9f8bde2b1dc4fb89361382dd4dba
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-11 23:11:24 +0530

    Script to create boilerplate

--- nodeJS/boilerplate.js
+++ nodeJS/boilerplate.js
@@ -1,14 +1,15 @@
 // Script to create Directory based on passed arguments with html css and js files.
 
 const fs = require('fs');
-const folderName= process.argv.slice(2)
+const path = require('path')
+const folderName= process.argv.slice(2);
 try{
 folderName.forEach(element => {
     fs.mkdirSync(element);
-    fs.writeFileSync(`${element}/index.html`)
-    fs.mkdirSync(`${element}/Assets`);
-    fs.writeFileSync(`${element}/Assets/app.css`) 
-    fs.writeFileSync(`${element}/Assets/app.js`) 
+    fs.mkdirSync(`${path.join(element)}/Assets`);
+    fs.writeFileSync(`${path.join(element,'index.html')}`,'');
+    fs.writeFileSync(`${path.join(element,'/Assets/app.css')}`,''); 
+    fs.writeFileSync(`${path.join(element,'/Assets/app.js')}`,''); 
 });
 }
 catch(e){
