a241d8f7faa0610661b3ba39f3ad7a7ab93af5bc
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-12 16:40:08 +0530

    Language Guesser

--- npm/languageGuesser/index.js
+++ npm/languageGuesser/index.js
@@ -1,6 +1,11 @@
+// Language Guess
 const argv=process.argv.slice(2);
 const franc=require('franc');
 const lang = require('langs')   
-argv.forEach(element => {
-console.log(lang.where('3',franc(element)).name);    
-});
+element=argv[0]
+const code=franc(element);
+
+if(code!='und')
+    console.log(lang.where('3',code).name);
+else
+    console.log('Could not Find a proper match. Try Another Sample');        
