f07d6f38613be93cd6c1084aa94119de7591702b
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-13 23:59:04 +0530

    updated

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -1,3 +1,10 @@
 const exp = require('express');
 const app=exp();
-console.die(app)
+
+app.use(()=>{
+    console.log('Something happened')
+})
+
+app.listen(3000,()=>{
+console.log('Listening on port 3000')
+})
