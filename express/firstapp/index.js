e33b62f65bc64e56e31e4f18978e9e094a66afc0
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-14 23:28:28 +0530

    updated

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -5,6 +5,10 @@ app.use(()=>{
     console.log('Something happened')
 })
 
+app.get("GOT YOUR REQUEST !",(req,res)=>{
+    console.log("Aye Aye, Seniore! ")
+})
+
 app.listen(3000,()=>{
 console.log('Listening on port 3000')
 })
