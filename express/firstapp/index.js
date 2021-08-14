debaee984fdef9b3a490cf7ee2ebfb83046d82bd
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-14 23:39:29 +0530

    updated

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -1,15 +1,17 @@
 // Getting Started with Express
 const exp = require('express');
 const app=exp();
+const port=process.argv[2]
 
-app.use(()=>{
-    console.log('Something happened')
-})
+    app.use(()=>{
+        console.log('Something happened')
+    })
+    
+    app.get("*",(req,res)=>{
+        return("Aye Aye, Seniore! ")
+    })
+    
+    app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+    })
 
-app.get("GOT YOUR REQUEST !",(req,res)=>{
-    console.log("Aye Aye, Seniore! ")
-})
-
-app.listen(3000,()=>{
-console.log('Listening on port 3000')
-})
