df9ccad1b6c8b7d500bc66ec29c478d541263620
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-17 18:47:00 +0530

    updated

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -7,7 +7,9 @@ if(port)
     //Every time a request is made use function is executed    
     app.use((req,res)=>{
         console.log('We got your request');
-        res.send({'color': 'green', 'name':'Xmen'});  
+        //res.send({'Hello, We got your request'});  
+        //res.send({'color': 'green', 'name':'Xmen'});  
+        res.send('<h1>Hello, We got your request</h1>');   
     })
     app.listen(port,()=>{
         console.log(`Listening on port ${port}`);
