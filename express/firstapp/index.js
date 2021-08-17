87637a1012e00a6bfe4d1f1d4b4b75353c6e5dc3
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-17 18:44:46 +0530

    updated

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -5,8 +5,9 @@ const port=process.argv[2]
 if(port)
 {
     //Every time a request is made use function is executed    
-    app.use(()=>{
+    app.use((req,res)=>{
         console.log('We got your request');
+        res.send({'color': 'green', 'name':'Xmen'});  
     })
     app.listen(port,()=>{
         console.log(`Listening on port ${port}`);
