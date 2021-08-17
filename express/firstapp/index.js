9d5fecdc6d6d5deed4ebf278c7eec530732829de
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-17 22:30:53 +0530

    updated

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -4,13 +4,43 @@ const app=exp();
 const port=process.argv[2]
 if(port)
 {
-    //Every time a request is made use function is executed    
+    /* //Every time a request is made use function is executed    
     app.use((req,res)=>{
         console.log('We got your request');
         //res.send({'Hello, We got your request'});  
         //res.send({'color': 'green', 'name':'Xmen'});  
         res.send('<h1>Hello, We got your request</h1>');   
     })
+ */
+
+    app.get("/r/:subreddit",(req,res) =>{
+        const {subreddit}=req.params;
+        console.log(`Browsing ${subreddit} Subreddit`);
+        res.send(`<h1>Browsing ${subreddit} Subreddit</h1>`)
+    })
+
+    app.get("/r/:subreddit/:postID",(req,res) =>{
+        const {subreddit,postID}=req.params;
+        console.log(req.params)
+        console.log(`Viewing ${postID} of ${subreddit} Subreddit`);
+        res.send(`<h1>Viewing ${postID} of ${subreddit} Subreddit</h1>`)
+    })
+
+    app.get("/cat",(req,res) =>{
+        console.log("Requested for /cat");
+        res.send("<h1>MEOW !!!!</h1>")
+    })
+
+    app.get("/dog",(req,res) =>{
+        console.log("Requested for /dog");
+        res.send("<h1>WOOF !!!!</h1>")
+    })
+
+    app.get("*",(req,res) =>{
+        console.log("Unknown request")
+        res.send("<h1>わからない</h1>")
+    })
+
     app.listen(port,()=>{
         console.log(`Listening on port ${port}`);
     })
