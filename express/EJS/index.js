eeb7fd70d4eb6a84f26c8e6629f44d49f954e5c7
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-19 00:02:45 +0530

    EJS

--- express/EJS/index.js
+++ express/EJS/index.js
@@ -2,10 +2,15 @@ const express =require('express');
 const app =express();
 const port = process.argv[2];
 
+// ejs is automatically required by express behind the scenes.
+// By default, When we us a view engine, express is going to assume 
+// that all our views/templates exist in a directory /views
+//                         or 
+// We can select a directory using views property of app.set()
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
-    res.send("Hi");
+    res.render('home');
 })
 
 app.listen(port, () =>{
