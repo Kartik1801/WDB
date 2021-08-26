2734b46af631a2ef68dae9c4b1fe00a37cbffb0f
kartik1801 <kartikdhawan1801@gmail.com>
2021-08-26 23:37:49 +0530

    updated

--- express/EJS/Reddit Demo/index.js
+++ express/EJS/Reddit Demo/index.js
@@ -3,10 +3,22 @@
 const express = require('express');
 const app=express();
 const path = require('path');
+
+const redditData=require('./data.json');
+console.log(redditData);
 let port= process.argv[2];
  if(!port){port=3000;}
 
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '/views'));
- 
+
+
+
+app.get("*",(req,res) => {
+    res.render('404')
+})
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`);
+})
